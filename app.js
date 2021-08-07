723142f1b8e888cba666bad5647bc7febcb9e631
edwin <edwinmathew63@gmail.com>
2021-08-07 15:37:42 +0530

    app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-test
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+rl.question("Enter csv file path ", function(name) {
+		console.log(name);
+		
+	});
